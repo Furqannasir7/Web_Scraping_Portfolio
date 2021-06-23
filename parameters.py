b029e44deba0016caec1518b34b7cc40e2391a22
Furqan Nasir <48833682+Furqannasir7@users.noreply.github.com>
2021-06-23 06:46:31 +0500

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -2,4 +2,4 @@ search_query = 'site:linkedin.com/in/ AND "python developer" AND "London"'
 file_name = 'results_file.csv'
 
 linkedin_username = 'nasirfurqan29@gmail.com'
-linkedin_password = '55552222BILLU'
+linkedin_password = '123456789'
